559913d5001487236ea227e41558526e34c88cdc
wjstopka <wjstopka@gmail.com>
2016-07-22 20:52:57 -0400

    Friday Javascript

---
+++ FridayNight/vacations/HelloWorld.js
@@ -0,0 +1,27 @@
+// var myNumber = 10;
+	// alert(myNumber);
+
+	// alert(myNumber)+10;
+	var myGreeting = "My name is ";
+	var myName = "Mud";
+		alert(myGreeting + myName);
+
+
+
+	// var myTrueBoolean = true;
+
+	// var myFalseBoolean = false;
+
+// var MyUndefined;
+
+	// alert(myTrueBoolean == myFalseBoolean);
+
+	// alert(myTrueBoolean == myTrueBoolean);
+
+
+// console.log(myGreeting + myName);
+
+// function helloWorld () {
+// 	var myGreeting = "My name is";
+// 	var myName = "Mud";
+// }
