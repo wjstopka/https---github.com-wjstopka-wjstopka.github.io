accec4fad8f56ab93435de9921d264045b92426b
wjstopka <wjstopka@gmail.com>
2016-07-26 01:20:39 -0400

    Tues Morning Javascript

--- javascriptexercise1/script.js
+++ javascriptexercise1/script.js
@@ -4,7 +4,6 @@ pageHeader.textContent = "javascript sports water";
 var sandwichImagesElems = document.getElementsByClassName("sandwichpic");
 console.log(sandwichImagesElems);
 
-
 var myButtons = document.getElementsByTagName("button");
 console.log(myButtons);
 
@@ -13,10 +12,21 @@ myButtons[0].addEventListener("click", function () {
 	sandwichImagesElems[0].src = "https://wswdelivery.com/wp-content/uploads/2015/05/gatorade-variety-550x515.jpg"
 	sandwichImagesElems[1].src = "http://cdn.bulbagarden.net/upload/thumb/c/ca/092Gastly.png/250px-092Gastly.png"
 	// alert("Ghastly likes Gatorade");
+
 });
 
 myButtons[1].addEventListener("click", function () {
 	var myImages = document.getElementsByTagName("img");
 	myImages[0].className = "juice";
 	myImages[1].className = "juice";
+});
+
+
+myButtons[1,0].addEventListener("click", function() {
+	
+var myJuice = document.getElementsByClassName("juice");
+	myJuice[0].src = "https://wswdelivery.com/wp-content/uploads/2015/05/gatorade-variety-550x515.jpg"
+	myJuice[1].src ="http://cdn.bulbagarden.net/upload/thumb/c/ca/092Gastly.png/250px-092Gastly.png"
+
+	
 })
